4fc886c920a8895696b8b1cd6f9dcc8845b60b51
rails <rails@rails>
2014-12-04 15:39:18 +0530

    authority added for the new link in index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+    @article = Article.new
   end
 
   def new
@@ -15,12 +16,21 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(articles_param)
+      redirect_to articles_path
+    end
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      redirect_to articles_path
+    end
   end
 
   private 
